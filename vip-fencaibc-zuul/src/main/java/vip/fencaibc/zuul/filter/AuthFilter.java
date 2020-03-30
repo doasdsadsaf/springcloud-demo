5d2c0e757bbfbaf333a55847867c9b407c5b1373
598644648@qq.com <598644648@qq.com>
2020-03-30 17:25:37 +0800

    实现通过网关-网关调用其他微服务功能

---
+++ vip-fencaibc-zuul/src/main/java/vip/fencaibc/zuul/filter/AuthFilter.java
@@ -0,0 +1,28 @@
+package vip.fencaibc.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class AuthFilter extends ZuulFilter {
+
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
